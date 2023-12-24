6f1103f7774caef6c565b96233d9c1198d73b8b8
log101 <ffrknerdm@gmail.com>
2023-12-24 13:36:58 +0000

    feat: add home page for events

--- src/pages/dj/index.tsx
+++ src/pages/dj/index.tsx
@@ -1,21 +1,44 @@
+import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { UserButton } from "@clerk/nextjs"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "Playground",
-  description: "The OpenAI Playground built using the components."
+  title: "DJ / İstek Müzik"
 }
 
 export default function DjHomePage() {
   return (
     <>
-      <div className='h-full flex-col md:flex'>
-        <div className='container flex flex-row items-center justify-between align-center py-4 md:h-16'>
+      <div className='h-screen flex flex-col'>
+        <div className='container flex flex-row items-center justify-between align-center py-4'>
           <h2 className='text-lg font-semibold'>İstek Müzik</h2>
           <UserButton afterSignOutUrl='/' />
         </div>
+
         <Separator />
+
+        <div className='container flex flex-col justify-between space-y-3 py-4 h-full'>
+          <div className='flex flex-col space-y-2'>
+            <h2 className='text-xl'>Oluşturduğun Etkinlikler</h2>
+            <div className='flex flex-row space-x-3 items-center'>
+              <div className='w-20 h-20 bg-slate-100 border rounded shadow-sm'></div>
+              <div className='flex flex-col space-y-2'>
+                <p className='text-lg'>Örnek Etkinlik</p>
+                <p className='text-lg font-light'>Etkinlik Henüz Başlamadı</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='flex flex-col space-y-2'>
+            <Button>
+              <p className='text-lg'>Etkinlik Oluştur</p>
+            </Button>
+            <Button variant='secondary' className='bg-blue-500 hover:bg-blue-400'>
+              <p className='text-lg'>Tavsiyelerini Düzenle</p>
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   )
