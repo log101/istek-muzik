ca38bc0c50d83161258e9cd5928f279d96e2a2f1
log101 <ffrknerdm@gmail.com>
2023-12-24 14:01:28 +0000

    feat: add event dialog

--- src/pages/dj/index.tsx
+++ src/pages/dj/index.tsx
@@ -1,4 +1,16 @@
 import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogClose
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { Separator } from "@/components/ui/separator"
 import { UserButton } from "@clerk/nextjs"
 import { Metadata } from "next"
@@ -31,10 +43,45 @@ export default function DjHomePage() {
           </div>
 
           <div className='flex flex-col space-y-2'>
-            <Button>
-              <p className='text-lg'>Etkinlik Oluştur</p>
-            </Button>
-            <Button variant='secondary' className='bg-blue-500 hover:bg-blue-400'>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button className='bg-green-600 hover:bg-green-500'>
+                  <p className='text-lg'>Etkinlik Oluştur</p>
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>
+                    <p className='text-lg'>Yeni Etkinlik Oluştur</p>
+                  </DialogTitle>
+                  <DialogDescription>
+                    Etkinlik ile alakalı detayları girdikten sonra kaydete bas. Kayıttan sonra etkinlik sayfasına
+                    yönlendirileceksin.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className='grid gap-4 py-4'>
+                  <div className='grid grid-cols-4 items-center gap-4'>
+                    <Label htmlFor='name' className='text-right'>
+                      Mekan Adı:
+                    </Label>
+                    <Input id='name' className='col-span-3' />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <div className='flex flex-col space-y-2'>
+                    <Button type='submit' className='bg-green-600 hover:bg-green-500'>
+                      <p className='text-lg'>Kaydet</p>
+                    </Button>
+                    <DialogClose asChild>
+                      <Button variant='secondary'>
+                        <p className='text-lg'>Vazgeç</p>
+                      </Button>
+                    </DialogClose>
+                  </div>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+            <Button variant='secondary' className='bg-blue-400 hover:bg-blue-300'>
               <p className='text-lg'>Tavsiyelerini Düzenle</p>
             </Button>
           </div>
