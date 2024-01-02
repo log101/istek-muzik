4498f49db5b38e4f8675c1fd91f8c6ba2d1b42f7
log101 <ffrknerdm@gmail.com>
2024-01-02 09:10:27 +0000

    feat: add event api

--- src/pages/api/events/[id].ts
+++ src/pages/api/events/[id].ts
@@ -1,9 +1,35 @@
+import { getAuth } from "@clerk/nextjs/server"
+import { PrismaClient } from "@prisma/client"
 import type { NextApiRequest, NextApiResponse } from "next"
 
-type ResponseData = {
-  message: string
-}
+const prisma = new PrismaClient()
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
-  res.status(200).json({ message: "Hello from Next.js!" })
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { userId } = getAuth(req)
+  const { id } = req.query
+
+  if (!userId) {
+    res.status(401).send({ error: "Unauthorized" })
+  } else {
+    if (req.method === "GET") {
+      return prisma.event
+        .findUnique({
+          where: {
+            id: Number(id)
+          }
+        })
+        .then(post => {
+          if (post) {
+            res.json(post)
+          } else {
+            res.status(404).send({ error: "There is no active event" })
+          }
+        })
+        .catch(() => {
+          res.status(500).send({ error: "Error getting event" })
+        })
+    } else {
+      res.status(400).send({ error: "HTTP Method Not Supported" })
+    }
+  }
 }
--- src/pages/events/[id].tsx
+++ src/pages/events/[id].tsx
@@ -1,10 +1,27 @@
+import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { UserButton } from "@clerk/nextjs"
 import { useRouter } from "next/router"
-import Script from "next/script"
+import useSWR, { Fetcher } from "swr"
+import { Event } from "@prisma/client"
+import Link from "next/link"
+
+const fetcher: Fetcher<Event, string> = async (url: string) => {
+  const res = await fetch(url)
+
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.")
+    throw error
+  }
+
+  return res.json()
+}
 
 const EventPage = () => {
   const router = useRouter()
+
+  // TODO: Add error handling
+  const { data } = useSWR(`/api/events/${router.query.id}`, fetcher)
 
   return (
     <>
@@ -16,26 +33,36 @@ const EventPage = () => {
 
         <Separator />
 
-        <div className='container flex flex-col space-y-3 py-4 h-full'>
-          <div id='qrcode'></div>
-          <div className='flex flex-col space-y-2'>Deneme {router.query.id}</div>
-        </div>
+        {data && (
+          <>
+            <div className='container flex flex-col space-y-8 py-4 h-full justify-center'>
+              {/* Show dj qr code */}
+              <div id='qrcode' className='flex justify-center'></div>
+              <div className='flex flex-col space-y-2'>
+                <div className='text-lg'>
+                  <span className='font-semibold'>Etkinliğin Mekanı:</span> {data.locationTitle}
+                </div>
+              </div>
+              <div className='flex flex-col space-y-3'>
+                <Button className='bg-green-600 hover:bg-green-500'>
+                  <p className='text-lg'>Etkinliği Başlat</p>
+                </Button>
+                <Button variant='secondary' className='bg-blue-400 hover:bg-blue-300'>
+                  <p className='text-lg'>Etkinliği Düzenle</p>
+                </Button>
+                <Button>
+                  <p className='text-lg'>Paylaş</p>
+                </Button>
+                <Button variant='secondary'>
+                  <Link href={`/dj`}>
+                    <p className='text-lg'>Geri Dön</p>
+                  </Link>
+                </Button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
-      <Script
-        src='https://cdn.jsdelivr.net/gh/davidshimjs/qrcodejs@gh-pages/qrcode.min.js'
-        onLoad={() =>
-          // @ts-expect-error next/script
-          new QRCode("qrcode", {
-            text: "http://jindo.dev.naver.com/collie",
-            width: 128,
-            height: 128,
-            colorDark: "#000000",
-            colorLight: "#ffffff",
-            // @ts-expect-error next/script
-            correctLevel: QRCode.CorrectLevel.H
-          })
-        }
-      />
     </>
   )
 }
