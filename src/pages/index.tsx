6cc42718f640023bbdad5c1ebf7df48f1024e7ed
log101 <ffrknerdm@gmail.com>
2024-01-16 09:29:36 +0300

    style: add spacing to place page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,9 @@ import Image from "next/image"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
@@ -24,8 +27,8 @@ export default function Home() {
 
       <Separator />
 
-      <div className='container flex flex-col space-y-3 py-4 h-full'>
-        <div className='flex flex-row items-center gap-4'>
+      <div className='container flex flex-col space-y-5 py-4 h-full'>
+        <div className='flex flex-row items-center gap-2'>
           <div className='w-[48px] h-[48px] bg-slate-300 rounded-full'></div>
           <div className='flex flex-col h-[48px] justify-between'>
             <p className='text-md'>Kızılay Mado</p>
@@ -33,16 +36,19 @@ export default function Home() {
           </div>
         </div>
 
-        <p>Hangi şarkı çalsın istersin?</p>
+        <div className='flex flex-col gap-2'>
+          <h4 className='scroll-m-20 text-xl font-semibold tracking-tight'>Hangi şarkı çalsın istersin?</h4>
 
-        <Input placeholder='Müzik Ara...' />
-        <Button className='w-full bg-green-600 hover:bg-green-500'>Müzik İste</Button>
+          <Input placeholder='Müzik Ara...' />
+          <Button className='w-full bg-green-600 hover:bg-green-500'>Müzik İste</Button>
+        </div>
 
-        <div className='flex flex-col gap-3'>
-          <div className='flex flex-row justify-between'>
-            <p className='text-md'>Sıradaki parça</p>
+        <div className='flex flex-col gap-2'>
+          <div className='flex flex-row justify-between items-center'>
+            <h4 className='scroll-m-20 text-xl font-semibold tracking-tight'>Sıradaki Parça</h4>
             <p className='text-sm text-muted-foreground underline'>bütün liste</p>
           </div>
+
           <div className='flex flex-row gap-4 justify-items-stretch'>
             <div className='w-[52px] h-[52px] bg-slate-300 rounded-lg'></div>
             <div>
@@ -53,6 +59,23 @@ export default function Home() {
             <p className='text-sm text-muted-foreground text-right flex-1 self-end'>@esra.key</p>
           </div>
         </div>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Tavsiyeler</CardTitle>
+            <CardDescription>
+              En çok çalınan, en popüler şarkılara göz at. İstek göndermek için şarkının üzerine tıklaman yeterli!
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className='flex flex-row justify-between'>
+              <div className='w-[52px] h-[52px] bg-slate-300 rounded-lg'></div>
+              <div className='w-[52px] h-[52px] bg-slate-300 rounded-lg'></div>
+              <div className='w-[52px] h-[52px] bg-slate-300 rounded-lg'></div>
+              <div className='w-[52px] h-[52px] bg-slate-300 rounded-lg'></div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </main>
   )
